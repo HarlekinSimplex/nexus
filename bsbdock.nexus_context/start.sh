40c46a8d8793d6c035534410818de70444faca64
Stephan Becker <stephan.becker@becker-systemberatung.de>
2022-03-20 00:23:45 +0100

    Update container creation

--- bsbdock.nexus_context/start.sh
+++ bsbdock.nexus_context/start.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 # Log reticulum interface status
+echo "#############################################################"
+echo "# Welcome to Nexus Server"
+echo "# A minimal broadcast messaging server based on Reticulum"
+echo "#############################################################"
+
+echo ""
+echo "# -----------------------------------------------------------"
+echo "# Actual Reticulum interface configuration:"
+echo "# -----------------------------------------------------------"
 rnstatus
 
+echo ""
+echo "# -----------------------------------------------------------"
+echo "# Nexus Server startup"
+echo "# -----------------------------------------------------------"
 # Launch Nexus Server with unbuffered logs (docker takes those logs)
 python3 -u /bsb/Nexus/NexusServer.py
+echo "# -----------------------------------------------------------"
+echo "# Nexus Server terminated"
+echo "# -----------------------------------------------------------"
+
+exit(0)
