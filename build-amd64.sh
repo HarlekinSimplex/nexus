7a06098ff2f4a12babb9ae05d51bc6813800126f
Stephan Becker <stephan.becker@becker-systemberatung.de>
2022-03-19 20:20:46 +0100

    Update container creation

--- build-amd64.sh
+++ build-amd64.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # docker buildx build --push --no-cache --tag bsbdock/nexus1.0_linux-amd64 -f Dockerfile_nexus1.0_linux-amd64 .
+cd ./bsbdock.nexus_context
 docker buildx build --tag bsbdock/nexus -f Dockerfile_nexus1.0_linux-amd64 .
