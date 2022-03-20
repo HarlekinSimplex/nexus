a5cdc7f38dc9e4f017264d932f3b39740b903ec9
Stephan Becker <stephan.becker@becker-systemberatung.de>
2022-03-20 20:50:59 +0100

    Update container creation

--- build-arm-v7.sh
+++ build-arm-v7.sh
@@ -5,5 +5,5 @@ cd ./bsbdock.nexus_context
 docker build --tag bsbdock/nexus -f Dockerfile_nexus1.0_linux-arm-v7 .
 cd ..
 
-#cd ./bsbdock.reticulum_context
-#docker build --tag bsbdock/nexus -f Dockerfile_reticulum2.1_linux-arm-v7 .
+cd ./bsbdock.reticulum_context
+docker build --tag bsbdock/nexus -f Dockerfile_reticulum2.1_linux-arm-v7 .
