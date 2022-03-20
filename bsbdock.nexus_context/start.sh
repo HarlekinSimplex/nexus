bf09af596dfc4ea3b54788f03764a0f59d38c625
Stephan Becker <stephan.becker@becker-systemberatung.de>
2022-03-20 01:40:03 +0100

    Update container creation

--- bsbdock.nexus_context/start.sh
+++ bsbdock.nexus_context/start.sh
@@ -14,7 +14,11 @@ echo "NEXUS_PORT=$NEXUS_PORT"
 echo "NEXUS_ASPECT=$NEXUS_ASPECT"
 echo "NEXUS_ROLE=$NEXUS_ROLE"
 echo "NEXUS_LONGPOLL=$NEXUS_LONGPOLL"
-echo ${NEXUS_CONFIG:+--config=NEXUS_CONFIG} ${NEXUS_PORT:+--port=NEXUS_PORT} ${NEXUS_ASPECT:+--port=NEXUS_ASPECT} ${NEXUS_ROLE:+--port=NEXUS_ROLE} ${NEXUS_LONGPOLL:+--port=NEXUS_LONGPOLL}
+echo ${NEXUS_CONFIG:+--config=$NEXUS_CONFIG} \
+${NEXUS_PORT:+--port=$NEXUS_PORT} \
+${NEXUS_ASPECT:+--aspect=$NEXUS_ASPECT} \
+${NEXUS_ROLE:+--role=NEXUS_ROLE} \
+${NEXUS_LONGPOLL:+--longpoll=NEXUS_LONGPOLL}
 echo ""
 echo "-------------------------------------------------------------"
 echo " Actual Reticulum interface configuration:"
@@ -26,7 +30,7 @@ echo "-------------------------------------------------------------"
 echo " Nexus Server startup"
 echo "-------------------------------------------------------------"
 # Launch Nexus Server with unbuffered logs (docker takes those logs)
-# python3 -u /bsb/Nexus/NexusServer.py ${NEXUS_CONFIG:+--config=NEXUS_CONFIG} ${NEXUS_PORT:+--port=NEXUS_PORT} ${NEXUS_ASPECT:+--port=NEXUS_ASPECT} ${NEXUS_ROLE:+--port=NEXUS_ROLE} ${NEXUS_LONGPOLL:+--port=NEXUS_LONGPOLL}
+# python3 -u /bsb/Nexus/NexusServer.py
 
 echo ""
 echo "-------------------------------------------------------------"
