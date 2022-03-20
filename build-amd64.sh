538c472849324b860c999f72098b0b063c4dab4a
Stephan Becker <stephan.becker@becker-systemberatung.de>
2022-03-20 21:37:07 +0100

    Update container creation

--- build-amd64.sh
+++ build-amd64.sh
@@ -2,11 +2,17 @@
 # docker push bsbdock/nexus:1.0_linux-amd64
 # docker push bsbdock/reticulum:2.1_linux-amd64
 
+#################################################
+# Build and tag Nexus container
+#
 cd ./bsbdock.nexus_context
 docker build --tag bsbdock/nexus:1.0_linux-amd64 -f Dockerfile_nexus1.0_linux-amd64 .
 docker tag bsbdock/nexus:1.0_linux-amd64 bsbdock/nexus
 cd ..
 
+#################################################
+# Build and tag of Reticulum only container
+#
 cd ./bsbdock.reticulum_context
 docker build --tag bsbdock/reticulum:2.1_linux-amd64 -f Dockerfile_reticulum2.1_linux-amd64 .
 docker tag bsbdock/reticulum:2.1_linux-amd64 bsbdock/reticulum
--- build-arm-v7.sh
+++ build-arm-v7.sh
@@ -2,12 +2,18 @@
 # docker push bsbdock/nexus:1.0_linux-arm-v7
 # docker push bsbdock/reticulum:2.1_linux-arm-v7
 
+#################################################
+# Build and tag Nexus container
+#
 cd ./bsbdock.nexus_context
 docker build --tag bsbdock/nexus:1.0_linux-arm-v7 -f Dockerfile_nexus1.0_linux-arm-v7 .
 docker tag bsbdock/nexus:1.0_linux-arm-v7 bsbdock/nexus
 cd ..
 
-cd ./bsbdock.reticulum_context
-docker build --tag bsbdock/reticulum:2.1_linux-arm-v7 -f Dockerfile_reticulum2.1_linux-arm-v7 .
-docker tag bsbdock/reticulum:2.1_linux-arm-v7 bsbdock/reticulum
-cd ..
+#################################################
+# Build and tag of Reticulum only container
+#
+#cd ./bsbdock.reticulum_context
+#docker build --tag bsbdock/reticulum:2.1_linux-arm-v7 -f Dockerfile_reticulum2.1_linux-arm-v7 .
+#docker tag bsbdock/reticulum:2.1_linux-arm-v7 bsbdock/reticulum
+#cd ..
