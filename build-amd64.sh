99be9a5851d3741d16f6599b685deee3fcf8de09
Stephan Becker <stephan.becker@becker-systemberatung.de>
2022-03-20 02:50:43 +0100

    Update container creation

--- build-amd64.sh
+++ build-amd64.sh
@@ -5,5 +5,5 @@ cd ./bsbdock.nexus_context
 docker buildx build --tag bsbdock/nexus -f Dockerfile_nexus1.0_linux-amd64 .
 cd ..
 
-# cd ./bsbdock.reticulum_context
-# docker buildx build --tag bsbdock/nexus -f Dockerfile_reticulum2.1_linux-amd64 .
+cd ./bsbdock.reticulum_context
+docker buildx build --tag bsbdock/nexus -f Dockerfile_reticulum2.1_linux-amd64 .
