da3184c0fd3222c733068c44406970beed1660d9
Stephan Becker <stephan.becker@becker-systemberatung.de>
2022-03-20 21:51:51 +0100

    Update container creation

--- build-amd64.sh
+++ build-amd64.sh
@@ -13,7 +13,7 @@ cd ..
 #################################################
 # Build and tag of Reticulum only container
 #
-#cd ./bsbdock.reticulum_context
-#docker build --tag bsbdock/reticulum:2.1_linux-amd64 -f Dockerfile_reticulum2.1_linux-amd64 .
-#docker tag bsbdock/reticulum:2.1_linux-amd64 bsbdock/reticulum
-#cd ..
+cd ./bsbdock.reticulum_context
+docker build --tag bsbdock/reticulum:2.1_linux-amd64 -f Dockerfile_reticulum2.1_linux-amd64 .
+docker tag bsbdock/reticulum:2.1_linux-amd64 bsbdock/reticulum
+cd ..
