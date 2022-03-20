315bfb97c086d1697ca42ae5ab0c6de5dca5c3b9
Stephan Becker <stephan.becker@becker-systemberatung.de>
2022-03-20 01:48:09 +0100

    Update container creation

--- bsbdock.nexus_context/start.sh
+++ bsbdock.nexus_context/start.sh
@@ -34,12 +34,7 @@ echo "-------------------------------------------------------------"
 echo " Nexus Server startup"
 echo "-------------------------------------------------------------"
 # Launch Nexus Server with unbuffered logs (docker takes those logs)
-python3 -u /bsb/Nexus/NexusServer.py \
-${NEXUS_CONFIG:+--config=$NEXUS_CONFIG} \
-${NEXUS_PORT:+--port=$NEXUS_PORT} \
-${NEXUS_ASPECT:+--aspect=$NEXUS_ASPECT} \
-${NEXUS_ROLE:+--role=$NEXUS_ROLE} \
-${NEXUS_LONGPOLL:+--longpoll=$NEXUS_LONGPOLL}
+python3 -u /bsb/Nexus/NexusServer.py ${NEXUS_CONFIG:+--config=$NEXUS_CONFIG} ${NEXUS_PORT:+--port=$NEXUS_PORT} ${NEXUS_ASPECT:+--aspect=$NEXUS_ASPECT} ${NEXUS_ROLE:+--role=$NEXUS_ROLE} ${NEXUS_LONGPOLL:+--longpoll=$NEXUS_LONGPOLL}
 
 echo ""
 echo "-------------------------------------------------------------"
