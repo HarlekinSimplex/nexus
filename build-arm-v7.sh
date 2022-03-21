609f44334103533d56f0d9bdc53960f54bb0a1db
Stephan Becker <stephan.becker@becker-systemberatung.de>
2022-03-21 21:28:40 +0100

    Use hash as destination address

--- build-arm-v7.sh
+++ build-arm-v7.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-# docker push bsbdock/nexus:1.0_linux-arm-v7
+# docker push bsbdock/nexus:1.2.0_linux-arm-v7
 # docker push bsbdock/reticulum:2.1_linux-arm-v7
 
 #################################################
 # Build and tag nexus_server container
 #
 cd ./bsbdock.nexus_context
-docker build --tag bsbdock/nexus:1.0_linux-arm-v7 -f Dockerfile_nexus1.0_linux-arm-v7 .
-docker tag bsbdock/nexus:1.0_linux-arm-v7 bsbdock/nexus
+docker build --tag bsbdock/nexus:1.2.0_linux-arm-v7 -f Dockerfile_nexus1.2.0_linux-arm-v7 .
+docker tag bsbdock/nexus:1.2.0_linux-arm-v7 bsbdock/nexus
 cd ..
 
 #################################################
