bf3eab9d3d15b9338ef8437fcf9d68ae79dc8c23
Stephan Becker <stephan.becker@becker-systemberatung.de>
2022-04-05 19:03:03 +0200

    Standardization

--- build-arm-v7.sh
+++ build-arm-v7.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
-# docker push bsbdock/nexus:1.3.0.1_linux-arm-v7
+# docker push bsbdock/nexus:1.3.0.2_linux-arm-v7
 # docker push bsbdock/reticulum:2.1_linux-arm-v7
 
 #################################################
 # Build and tag nexus_server2 container
 #
 cd ./bsbdock.nexus_context
-docker build --build-arg CACHEBUST=$(date +%s) --tag bsbdock/nexus:1.3.0.1_linux-arm-v7 -f Dockerfile_nexus1.3.0.1_linux-arm-v7 .
-docker tag bsbdock/nexus:1.3.0.1_linux-arm-v7 bsbdock/nexus
+docker build --build-arg CACHEBUST=$(date +%s) --tag bsbdock/nexus:1.3.0.2_linux-arm-v7 -f Dockerfile_nexus1.3.0.2_linux-arm-v7 .
+docker tag bsbdock/nexus:1.3.0.2_linux-arm-v7 bsbdock/nexus
 cd ..
-
-#################################################
-# Build and tag of Reticulum only container
-#
-#cd ./bsbdock.reticulum_context
-#docker build --tag bsbdock/reticulum:2.1_linux-arm-v7 -f Dockerfile_reticulum2.1_linux-arm-v7 .
-#docker tag bsbdock/reticulum:2.1_linux-arm-v7 bsbdock/reticulum
-#cd ..
