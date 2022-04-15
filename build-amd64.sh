688a8877bc44ff639a5bdd908348b808ee1a7677
Stephan Becker <stephan.becker@becker-systemberatung.de>
2022-04-15 18:25:09 +0200

    Release 1.3.0.3

--- build-amd64.sh
+++ build-amd64.sh
@@ -5,6 +5,6 @@
 # Build and tag nexus_server2 container
 #
 cd ./bsbdock.nexus_context
-docker build --build-arg CACHEBUST=$(date +%s) --tag bsbdock/nexus:1.3.0.3_linux-amd64 -f Dockerfile_nexus1.3.0.3_linux-amd64 .
+docker build --no-cache --build-arg CACHEBUST=$(date +%s) --tag bsbdock/nexus:1.3.0.3_linux-amd64 -f Dockerfile_nexus1.3.0.3_linux-amd64 .
 docker tag bsbdock/nexus:1.3.0.3_linux-amd64 bsbdock/nexus
 cd ..
--- build-arm-v7.sh
+++ build-arm-v7.sh
@@ -5,6 +5,6 @@
 # Build and tag nexus_server container
 #
 cd ./bsbdock.nexus_context
-docker build --build-arg CACHEBUST=$(date +%s) --tag bsbdock/nexus:1.3.0.3_linux-arm-v7 -f Dockerfile_nexus1.3.0.3_linux-arm-v7 .
+docker build --no-cache --build-arg CACHEBUST=$(date +%s) --tag bsbdock/nexus:1.3.0.3_linux-arm-v7 -f Dockerfile_nexus1.3.0.3_linux-arm-v7 .
 docker tag bsbdock/nexus:1.3.0.3_linux-arm-v7 bsbdock/nexus
 cd ..
