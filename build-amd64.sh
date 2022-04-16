ee48ce0d4559302f3c33329599e09c69829803ef
Stephan Becker <stephan.becker@becker-systemberatung.de>
2022-04-16 16:10:34 +0200

    Release 1.4.0.0 (Build Spripts)

--- build-amd64.sh
+++ build-amd64.sh
@@ -8,6 +8,6 @@ ARCH=amd64
 
 cd ./bsbdock.nexus_context
 docker build --no-cache --build-arg CACHEBUST=$(date +%s) --tag bsbdock/nexus:$VER\_$OS-$ARCH -f Dockerfile_nexus$VER\_$OS-$ARCH .
-docker tag bsbdock/nexus:$VER_$OS-$ARCH bsbdock/nexus
-docker push bsbdock/nexus:$VER_$OS-$ARCH
+docker tag bsbdock/nexus:$VER\_$OS-$ARCH bsbdock/nexus
+docker push bsbdock/nexus:$VER\_$OS-$ARCH
 cd ..
