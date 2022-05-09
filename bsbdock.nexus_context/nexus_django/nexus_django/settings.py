00f91bcf8db881f7ff2e68f036c89d093dace86b
Stephan Becker <stephan.becker@becker-systemberatung.de>
2022-05-09 22:33:16 +0200

    Django Production Build

--- bsbdock.nexus_context/nexus_django/nexus_django/settings.py
+++ bsbdock.nexus_context/nexus_django/nexus_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mm^ptjwdo4qul=t-=5i_s$*0vthaaz-^^q2ykc3inmsh2bw-9j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
