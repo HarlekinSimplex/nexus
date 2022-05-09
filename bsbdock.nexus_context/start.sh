fce359f5ca0ae77d72778726aef59aec065481e8
Stephan Becker <stephan.becker@becker-systemberatung.de>
2022-05-09 12:21:13 +0200

    Django Production Build

--- bsbdock.nexus_context/start.sh
+++ bsbdock.nexus_context/start.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # USE the trap if you need to also do manual cleanup after the service is stopped,
 #     or need to start multiple services in the one container
 trap "echo TRAPed signal" HUP INT QUIT TERM
