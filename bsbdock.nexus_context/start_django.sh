fe50ec977e3d2e050c3cb39c695fffaacc714a63
Stephan Becker <stephan.becker@becker-systemberatung.de>
2022-05-09 17:26:54 +0200

    Django Production Build

--- bsbdock.nexus_context/start_django.sh
+++ bsbdock.nexus_context/start_django.sh
@@ -25,7 +25,7 @@ echo ""
 echo "-------------------------------------------------------------"
 echo " Actual Reticulum interface configuration:"
 echo "-------------------------------------------------------------"
-cat /root/.reticulum/config
+cat .reticulum/config
 
 echo ""
 echo "-------------------------------------------------------------"
