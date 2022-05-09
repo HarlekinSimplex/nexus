bc0d3ded669fdba4212f61f0605825460236e8c9
Stephan Becker <stephan.becker@becker-systemberatung.de>
2022-05-09 14:58:39 +0200

    Django Production Build

--- bsbdock.nexus_context/entrypoint.sh
+++ bsbdock.nexus_context/entrypoint.sh
@@ -10,15 +10,15 @@ if [ "$MAP_NODE_UID" != "no" ]; then
     uid=$(stat -c '%u' "$MAP_NODE_UID")
     gid=$(stat -c '%g' "$MAP_NODE_UID")
 
-    echo "dev ---> UID = $uid / GID = $gid"
+    echo "bsb ---> UID = $uid / GID = $gid"
 
-    export USER=dev
+    export USER=bsb
 
-    usermod -u $uid dev 2> /dev/null && {
-      groupmod -g $gid dev 2> /dev/null || usermod -a -G $gid dev
+    usermod -u $uid bsb 2> /dev/null && {
+      groupmod -g $gid bsb 2> /dev/null || usermod -a -G $gid bsb
     }
 fi
 
-echo "**** GOSU dev $@ ..."
+echo "**** GOSU bsb $@ ..."
 
-exec /usr/local/bin/gosu dev "$@"
+exec /usr/local/bin/gosu bsb "$@"
