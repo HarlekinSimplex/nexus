5aea661e7de5106ee3b0b4772812a9379e9d2cce
Stephan Becker <stephan.becker@becker-systemberatung.de>
2022-05-10 11:09:14 +0200

    Django Server RNS Test

---
+++ bsbdock.nexus_context/nexus_django/server/apps.py
@@ -0,0 +1,22 @@
+import RNS
+from django.apps import AppConfig
+
+
+class ServerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'server'
+
+    def ready(self):
+        # Pull up Reticulum stack as configured
+        RNS.Reticulum()
+
+        # Startup log with used parameter
+        RNS.log(" ____   _____ ____   _   _                      _____", RNS.LOG_INFO)
+        RNS.log("|  _ \\ / ____|  _ \\ | \\ | |                    / ____|", RNS.LOG_INFO)
+        RNS.log("| |_) | (___ | |_) ||  \\| | _____  ___   _ ___| (___   ___ _ ____   _____ _ __", RNS.LOG_INFO)
+        RNS.log("|  _ < \\___ \\|  _ < | . ` |/ _ \\ \\/ / | | / __|\\___ \\ / _ \\ '__\\ \\ / / _ \\ '__|", RNS.LOG_INFO)
+        RNS.log("| |_) |____) | |_) || |\\  |  __/>  <| |_| \\__ \\____) |  __/ |   \\ V /  __/ |", RNS.LOG_INFO)
+        RNS.log("|____/|_____/|____(_)_| \\_|\\___/_/\\_\\\\__,_|___/_____/ \\___|_|    \\_/ \\___|_|", RNS.LOG_INFO)
+        RNS.log("", RNS.LOG_INFO)
+        RNS.log("Copyright (c) 2022 Stephan Becker / Becker-Systemberatung, MIT License", RNS.LOG_INFO)
+        RNS.log("...............................................................................", RNS.LOG_INFO)
