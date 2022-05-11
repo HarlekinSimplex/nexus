e11aac5bc5993995695e80e6345c61c05fb32c75
Stephan Becker <stephan.becker@becker-systemberatung.de>
2022-05-11 12:43:24 +0200

    Django Server RNS Test - Intial Server App with Distribution and Bridge Traget Model

--- build.sh
+++ build.sh
@@ -5,16 +5,14 @@
 
 IMAGE_ARCH=$1
 IMAGE_VERSION=$2
-if [ "$IMAGE_ARCH" == "dev" ]
-then
+if [ "$IMAGE_ARCH" == "dev" ] ; then
   IMAGE_VERSION=$IMAGE_ARCH
   IMAGE_ARCH=
 fi
 
 # Check
 if [ "$IMAGE_ARCH" ] && [ "$IMAGE_ARCH" != "amd64" ] && [ "$IMAGE_ARCH" != "arm64" ] && [ "$IMAGE_ARCH" != "arm" ] ||
-   [ "$1" == "?" ] || [ "$1" == "-h" ] || [ "$1" == "--help" ]
-then
+   [ "$1" == "?" ] || [ "$1" == "-h" ] || [ "$1" == "--help" ] ; then
   echo ""
   echo "Usage:"
   echo "  build [<Arch>=amd64|arm64|arm] [<Version>=dev]"
@@ -33,21 +31,39 @@ IMAGE_ARCH="${IMAGE_ARCH:-amd64}"
 IMAGE_OS=linux
 IMAGE_TAG="$IMAGE_VERSION"_"$IMAGE_OS"-"$IMAGE_ARCH"
 
-echo "$IMAGE_TAG"
+echo "Using $IMAGE_TAG as image tag."
 
-exit
-
+# Move into image context
 cd ./bsbdock.nexus_context || exit
-docker build --no-cache --build-arg CACHEBUST="$(date +%s)" \
-  --tag bsbdock/nexus:"$IMAGE_TAG" \
-  -f Dockerfile_nexus_"$IMAGE_TAG" .
-docker tag bsbdock/nexus:"$IMAGE_TAG" bsbdock/nexus:"$IMAGE_VERSION"
 
-if [ "$IMAGE_VERSION" != "dev" ]
-then
-  docker tag bsbdock/nexus:"$IMAGE_TAG" bsbdock/nexus
-  docker push bsbdock/nexus:"$IMAGE_TAG"
+# Check if Dockerfile exists
+FILE=Dockerfile_nexus_"$IMAGE_TAG"
+if test -f "$FILE"; then
+    echo "Dockerfile $FILE to build image exists."
+    echo "Building image ..."
+
+    # Build image according given build parameters
+    docker build --no-cache --build-arg CACHEBUST="$(date +%s)" \
+      --tag bsbdock/nexus:"$IMAGE_TAG" \
+      -f Dockerfile_nexus_"$IMAGE_TAG" .
+
+    # Tag actual build image with given version
+    docker tag bsbdock/nexus:"$IMAGE_TAG" bsbdock/nexus:"$IMAGE_VERSION"
+
+    # Non dev image builds will be tagged as :latest and pushed to GitHub
+    if [ "$IMAGE_VERSION" != "dev" ]
+    then
+      docker tag bsbdock/nexus:"$IMAGE_TAG" bsbdock/nexus
+      docker push bsbdock/nexus:"$IMAGE_TAG"
+    fi
+
+    RESULT=$?
+else
+    echo "Dockerfile $FILE to build image does NOT exists."
+    echo "Image build aborted..."
+    RESULT=1
 fi
 
-cd .. || exit
-
+# Return from context
+cd .. || exit $RESULT
+exit $RESULT
