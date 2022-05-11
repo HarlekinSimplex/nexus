4adfcab9ee6b2620c59e04fb2a32927a637186cd
Stephan Becker <stephan.becker@becker-systemberatung.de>
2022-05-11 12:58:23 +0200

    Django Server RNS Test - Intial Server App with Distribution and Bridge Traget Model

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ RED='\033[0;31m'
 YELLOW='\033[1;33m'
 GREEN='\033[0;32m'
 BLUE='\033[0;34m'
-CYAN='\033[0;36m'
+LIGHTBLUE='\033[1;34m'
 NC='\033[0m' # No Color
 
 IMAGE_ARCH=$1
@@ -46,7 +46,7 @@ cd ./bsbdock.nexus_context || exit
 # Check if Dockerfile exists
 FILE=Dockerfile_nexus_"$IMAGE_TAG"
 if test -f "$FILE"; then
-    echo -e "${GREEN}Dockerfile ${CYAN}$FILE${GREEN} to build image exists.${NC}"
+    echo -e "${GREEN}Dockerfile ${LIGHTBLUE}$FILE${GREEN} to build image exists.${NC}"
     echo -e "${BLUE}Building image ...${NC}"
 
 
