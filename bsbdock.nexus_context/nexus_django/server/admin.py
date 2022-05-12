15f75a087dfd1b51242701c8aac6a77c40cb60e8
Stephan Becker <stephan.becker@becker-systemberatung.de>
2022-05-12 13:44:45 +0200

    Django Server RNS Test - New generic build script

--- bsbdock.nexus_context/nexus_django/server/admin.py
+++ bsbdock.nexus_context/nexus_django/server/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+# Register your models here.
+from .models import DistributionTargets, BridgeTargets
 
 admin.site.register(DistributionTargets)
 admin.site.register(BridgeTargets)
