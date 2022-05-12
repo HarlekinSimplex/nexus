d3436dd72ed0e74792fd1fae9d8e3226d478fd67
Stephan Becker <stephan.becker@becker-systemberatung.de>
2022-05-12 17:38:03 +0200

    Django Server RNS Test - New generic build script

--- bsbdock.nexus_context/start_django.sh
+++ bsbdock.nexus_context/start_django.sh
@@ -19,9 +19,9 @@ export NC='\033[0m' # No Color
 
 # Log reticulum interface status
 echo -e "${LIGHT_BLUE}"
-echo -e "-------------------------------------------------------------"
-echo -e "Startup of Nexus Server [Django]"
-echo -e "-------------------------------------------------------------"
+echo -e "${LIGHT_BLUE}-------------------------------------------------------------"
+echo -e "${LIGHT_BLUE}Startup of Nexus Server [Django]"
+echo -e "${LIGHT_BLUE}-------------------------------------------------------------"
 echo -e "${NC}"
 
 echo -e ""
