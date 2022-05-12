2a9de46500db50c6932ce7ba2437030691b710c7
Stephan Becker <stephan.becker@becker-systemberatung.de>
2022-05-12 13:31:47 +0200

    Django Server RNS Test - New generic build script

--- bsbdock.nexus_context/nexus_django/gunicorn.config.py
+++ bsbdock.nexus_context/nexus_django/gunicorn.config.py
@@ -230,17 +230,14 @@ def worker_abort(worker):
 def on_starting(server):
     server.log.info("BSB: Starting")
 
-
-'''
     # Pull up Reticulum stack as configured
     RNS.Reticulum()
 
-    RNS.log(" ____   _____ ____   _   _                      _____", RNS.LOG_INFO)
-    RNS.log("|  _ \\ / ____|  _ \\ | \\ | |                    / ____|", RNS.LOG_INFO)
-    RNS.log("| |_) | (___ | |_) ||  \\| | _____  ___   _ ___| (___   ___ _ ____   _____ _ __", RNS.LOG_INFO)
-    RNS.log("|  _ < \\___ \\|  _ < | . ` |/ _ \\ \\/ / | | / __|\\___ \\ / _ \\ '__\\ \\ / / _ \\ '__|", RNS.LOG_INFO)
-    RNS.log("| |_) |____) | |_) || |\\  |  __/>  <| |_| \\__ \\____) |  __/ |   \\ V /  __/ |", RNS.LOG_INFO)
-    RNS.log("|____/|_____/|____(_)_| \\_|\\___/_/\\_\\\\__,_|___/_____/ \\___|_|    \\_/ \\___|_|", RNS.LOG_INFO)
-    RNS.log("", RNS.LOG_INFO)
-    RNS.log("Copyright (c) 2022 Stephan Becker / Becker-Systemberatung, MIT License", RNS.LOG_INFO)
-'''
+    server.log.info(" ____   _____ ____   _   _                      _____")
+    server.log.info("|  _ \\ / ____|  _ \\ | \\ | |                    / ____|")
+    server.log.info("| |_) | (___ | |_) ||  \\| | _____  ___   _ ___| (___   ___ _ ____   _____ _ __")
+    server.log.info("|  _ < \\___ \\|  _ < | . ` |/ _ \\ \\/ / | | / __|\\___ \\ / _ \\ '__\\ \\ / / _ \\ '__|")
+    server.log.info("| |_) |____) | |_) || |\\  |  __/>  <| |_| \\__ \\____) |  __/ |   \\ V /  __/ |")
+    server.log.info("|____/|_____/|____(_)_| \\_|\\___/_/\\_\\\\__,_|___/_____/ \\___|_|    \\_/ \\___|_|")
+    server.log.info("")
+    server.log.info("Copyright (c) 2022 Stephan Becker / Becker-Systemberatung, MIT License")
