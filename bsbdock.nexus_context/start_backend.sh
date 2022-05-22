71042a168f1efaef2de69e43b20180f679efaa6d
Stephan Becker <stephan.becker@becker-systemberatung.de>
2022-05-22 18:31:53 +0200

    Alsa Integration

--- bsbdock.nexus_context/start_backend.sh
+++ bsbdock.nexus_context/start_backend.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+if [ "$DIREWOLF_AUTOSTART" != "False" ] ; then
+  echo -e ""
+  echo -e "-------------------------------------------------------------"
+  echo -e "Autostart Direwolf"
+  echo -e "-------------------------------------------------------------"
+  direwolf -t 0 -c "$DIREWOLF_CONFIG"/direwolf.conf &
+  echo "direwolf PID=""$(pgrep direwolf)"
+fi
+
+if [ "$SOUNDMODEM_AUTOSTART" != "False" ] ; then
+  echo -e ""
+  echo -e "-------------------------------------------------------------"
+  echo -e "Autostart Soundmodem"
+  echo -e "-------------------------------------------------------------"
+  sudo soundmodem "$SOUNDMODEM_CONFIG"/soundmodem.conf &
+  echo "soundmodem PID=""$(pgrep soundmodem)"
+fi
+
 echo -e ""
 echo -e "-------------------------------------------------------------"
 echo -e "Actual Reticulum interface configuration:"
@@ -33,24 +51,6 @@ if [ "$NOMADNET_AUTOSTART" != "False" ] ; then
   nomadnet --daemon &
   sleep 1
   echo "nomadnet PID="$(pgrep nomadnet)
-fi
-
-if [ "$DIREWOLF_AUTOSTART" != "False" ] ; then
-  echo -e ""
-  echo -e "-------------------------------------------------------------"
-  echo -e "Autostart Direwolf"
-  echo -e "-------------------------------------------------------------"
-  direwolf -t 0 -c "$DIREWOLF_CONFIG"/direwolf.conf &
-  echo "direwolf PID=""$(pgrep direwolf)"
-fi
-
-if [ "$SOUNDMODEM_AUTOSTART" != "False" ] ; then
-  echo -e ""
-  echo -e "-------------------------------------------------------------"
-  echo -e "Autostart Soundmodem"
-  echo -e "-------------------------------------------------------------"
-  sudo soundmodem "$SOUNDMODEM_CONFIG"/soundmodem.conf &
-  echo "soundmodem PID=""$(pgrep soundmodem)"
 fi
 
 echo -e ""
