b7ad23b4ff33a13c16928b0e399656ec2e28249c
Stephan Becker <stephan.becker@becker-systemberatung.de>
2022-05-28 16:35:13 +0200

    Direwolf additional options

--- bsbdock.nexus_context/start_backend.sh
+++ bsbdock.nexus_context/start_backend.sh
@@ -15,7 +15,10 @@ if [ "$DIREWOLF_AUTOSTART" != "False" ] ; then
   echo -e ""
   echo -e "${LIGHT_BLUE}Start direwolf:${NC}"
   echo -e "Using configuration from ${LIGHT_GREEN}$DIREWOLF_CONFIG/$DIREWOLF_INTERFACE_NAME.conf${NC}"
-  su bsb -c "direwolf -t 0 -p -c $DIREWOLF_CONFIG/$DIREWOLF_INTERFACE_NAME.conf -l $DIREWOLF_CONFIG/log &"
+  su bsb -c "direwolf -t 0 -p \
+    -c $DIREWOLF_CONFIG/$DIREWOLF_INTERFACE_NAME.conf \
+    -l $DIREWOLF_CONFIG/log \
+    $DIREWOLF_OPTIONS &"
   sleep 1
   echo "direwolf PID=""$(pgrep direwolf)"
 fi
