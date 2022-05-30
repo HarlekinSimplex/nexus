182596cf031d30ef4ebf611da80d6447ba50b83f
Stephan Becker <stephan.becker@becker-systemberatung.de>
2022-05-30 23:36:27 +0200

    Multiple TCP/IP Kiss interface konfiguration

--- nexus_root/.direwolf/_Templates/USBAudioCODEC.sh
+++ nexus_root/.direwolf/_Templates/USBAudioCODEC.sh
@@ -2,30 +2,25 @@
 ###############################################################################
 # Audio setup script for 'USB Audio CODEC' type devices
 #
+# $1 contains the naked dw instance name as specified by the .env or entrypoint.sh
+# It is used to pull in defaults to be used for this usb sound board
 
 # Use these environment variables in .env to setup device parameters for this backend startup script
-# Direwolf default interface name
 #DIREWOLF_INTERFACE_NAME=<Interface Name>
 #
 # Direwolf default interface device configuration
-#DIREWOLF_SOUND_CARD=1
+#DIREWOLF_<dw-instance>_SOUND_CARD=1
 #
 # Direwolf default amixer content values
-#DIREWOLF_PCM_PLAYBACK_SWITCH=on
-#DIREWOLF_PCM_PLAYBACK_VOLUME=80%
+#DIREWOLF_<dw-instance>_PCM_PLAYBACK_SWITCH=on
+#DIREWOLF_<dw-instance>_PCM_PLAYBACK_VOLUME=80%
 
 # Set sound card default
-export DIREWOLF_SOUND_CARD="${DIREWOLF_SOUND_CARD:-1}"
-# Check if we have a dual device configuration set up
-# Default is 'No'
-export DIREWOLF_DUAL_SOUND_CARD="${DIREWOLF_DUAL_SOUND_CARD:-No}"
-# Set dual device config defaults
-export DIREWOLF_INPUT_SOUND_CARD="${DIREWOLF_INPUT_SOUND_CARD:-$DIREWOLF_SOUND_CARD}"
-export DIREWOLF_OUTPUT_SOUND_CARD="${DIREWOLF_OUTPUT_SOUND_CARD:-$DIREWOLF_SOUND_CARD}"
+DIREWOLF_SOUND_CARD="${DIREWOLF_$1_SOUND_CARD:-1}"
 
 # Set Output defaults for USB AUdio CODEC
-export DIREWOLF_PCM_PLAYBACK_SWITCH="${DIREWOLF_PCM_PLAYBACK_SWITCH:-on}"
-export DIREWOLF_PCM_PLAYBACK_VOLUME="${DIREWOLF_PCM_PLAYBACK_VOLUME:-80%}"
+DIREWOLF_PCM_PLAYBACK_SWITCH="${DIREWOLF_$1_PCM_PLAYBACK_SWITCH:-on}"
+DIREWOLF_PCM_PLAYBACK_VOLUME="${DIREWOLF_$1_PCM_PLAYBACK_VOLUME:-80%}"
 # Set Output defaults
 # - Intentionally left blank -
 
@@ -39,10 +34,10 @@ aplay -l
 
 # Set some defaults
 echo -e ""
-echo -e "${LIGHT_BLUE}ALSA sound device selection:${NC}"
+echo -e "${LIGHT_BLUE}ALSA sound device selection for direwolf instance $1:${NC}"
 echo -e "DIREWOLF_SOUND_CARD=$DIREWOLF_SOUND_CARD"
 echo -e ""
-echo -e "${LIGHT_BLUE}Sound output channel parameters:${NC}"
+echo -e "${LIGHT_BLUE}Sound output channel parameters for direwolf instance $1:${NC}"
 echo -e "DIREWOLF_PCM_PLAYBACK_SWITCH=$DIREWOLF_PCM_PLAYBACK_SWITCH"
 echo -e "DIREWOLF_PCM_PLAYBACK_VOLUME=$DIREWOLF_PCM_PLAYBACK_VOLUME"
 
