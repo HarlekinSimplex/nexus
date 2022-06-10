d89e1b096caa522a9a1affd5191459aac5e35117
Stephan Becker <stephan.becker@becker-systemberatung.de>
2022-06-10 18:34:54 +0200

    repull.sh with target filter

--- repull.sh
+++ repull.sh
@@ -2,6 +2,11 @@
 #################################################
 # Reset actual repo state and repull from remote
 #
-sudo chown -R $(whoami) nexus*
+sudo chown -R "$(whoami)" nexus*
 git reset --hard
 git pull
+
+TARGET=${1-default}
+
+grep "$TARGET" .env_master | sed "s/$TARGET://g" >.env
+
