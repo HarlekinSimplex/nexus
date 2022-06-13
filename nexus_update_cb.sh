96b0b556850c042142b6f111d8312840ad9639c7
Stephan Becker <stephan.becker@becker-systemberatung.de>
2022-06-13 14:31:47 +0200

    Watch for Twins

--- nexus_update_cb.sh
+++ nexus_update_cb.sh
@@ -17,26 +17,32 @@ export CYAN='\033[0;36m'
 export LIGHT_CYAN='\033[1;36m'
 export NC='\033[0m' # No Color
 
-echo -e "${LIGHT_CYAN}Update nexus instance ${LIGHT_PURPLE}nexus-reticulum${NC}"
+echo -e "${LIGHT_BLUE}Update nexus instance ${LIGHT_GREEN}nexus-reticulum${NC}"
 cd ~/*reticulum
+pwd
 bash nexus_update.sh cb_reticulum
 
-echo -e "${LIGHT_CYAN}Update nexus instance ${LIGHT_PURPLE}nexus-home${NC}"
+echo -e "${LIGHT_BLUE}Update nexus instance ${LIGHT_GREEN}nexus-home${NC}"
 cd ~/*home
+pwd
 bash nexus_update.sh cb_home
 
-echo -e "${LIGHT_CYAN}Update nexus instance ${LIGHT_PURPLE}nexus-delta${NC}"
+echo -e "${LIGHT_BLUE}Update nexus instance ${LIGHT_GREEN}nexus-delta${NC}"
 cd ~/*delta
+pwd
 bash nexus_update.sh cb_delta
 
-echo -e "${LIGHT_CYAN}Update nexus instance ${LIGHT_PURPLE}nexus-cockpit${NC}"
+echo -e "${LIGHT_BLUE}Update nexus instance ${LIGHT_GREEN}nexus-cockpit${NC}"
 cd ~/*cockpit
+pwd
 bash nexus_update.sh cb_cockpit
 
-echo -e "${LIGHT_CYAN}Update nexus instance ${LIGHT_PURPLE}nexus-dev${NC}"
+echo -e "${LIGHT_BLUE}Update nexus instance ${LIGHT_GREEN}nexus-dev${NC}"
 cd ~/*dev
+pwd
 bash nexus_update.sh cb_dev
 
-echo -e "${LIGHT_CYAN}Update nexus instance ${LIGHT_PURPLE}nexus-hub${NC}"
+echo -e "${LIGHT_BLUE}Update nexus instance ${LIGHT_GREEN}nexus-hub${NC}"
 cd ~/*hub
+pwd
 bash nexus_update.sh cb_hub
