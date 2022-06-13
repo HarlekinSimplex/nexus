e14c2433faff179106f931fdf9882cede8f4b87a
Stephan Becker <stephan.becker@becker-systemberatung.de>
2022-06-13 11:47:07 +0200

    New container reload script

---
+++ colors.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#################################################
+# Check terminal colors
+#
+
+
+export RED='\033[0;31m'
+export LIGHT_RED='\033[1;31m'
+export GREEN='\033[0;32m'
+export LIGHT_GREEN='\033[1;32m'
+export YELLOW='\033[0;33m'
+export LIGHT_YELLOW='\033[1;33m'
+export BLUE='\033[0;34m'
+export LIGHT_BLUE='\033[1;34m'
+export PURPLE='\033[0;35m'
+export LIGHT_PURPLE='\033[1;35m'
+export CYAN='\033[0;36m'
+export LIGHT_CYAN='\033[1;36m'
+export NC='\033[0m' # No Color
+
+echo -e "${RED}The quick brown fox jumps over the lazy dog${NC}"
+echo -e "${LIGHT_RED}The quick brown fox jumps over the lazy dog${NC}"
+echo -e "${GREEN}The quick brown fox jumps over the lazy dog${NC}"
+echo -e "${LIGHT_GREEN}The quick brown fox jumps over the lazy dog${NC}"
+echo -e "${YELLOW}The quick brown fox jumps over the lazy dog${NC}"
+echo -e "${LIGHT_YELLOW}The quick brown fox jumps over the lazy dog${NC}"
+echo -e "${LIGHT_BLUE}The quick brown fox jumps over the lazy dog${NC}"
+echo -e "${PURPLE}The quick brown fox jumps over the lazy dog${NC}"
+echo -e "${LIGHT_PURPLE}The quick brown fox jumps over the lazy dog${NC}"
+echo -e "${CYAN}The quick brown fox jumps over the lazy dog${NC}"
+echo -e "${LIGHT_CYAN}The quick brown fox jumps over the lazy dog${NC}"
