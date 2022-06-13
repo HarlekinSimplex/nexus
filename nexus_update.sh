2ec34b6d87861e677de5ac51df8664c306497edc
Stephan Becker <stephan.becker@becker-systemberatung.de>
2022-06-13 14:40:07 +0200

    Watch for Twins

--- nexus_update.sh
+++ nexus_update.sh
@@ -7,13 +7,13 @@ source colors_set.sh
 # If no template name to be used to create .env use 'default'
 TEMPLATE=${1-default}
 
+# Shut down actual container composition
+echo -e "${PURPLE}Shut down actual container composition${NC}"
+docker compose down
+
 # Pull repo from git
 echo -e "${PURPLE}Pull actual repo update from git and create .env from template '$TEMPLATE'${NC}"
 bash nexus_pull.sh "$TEMPLATE"
-
-# Shut down container composition
-echo -e "${PURPLE}Shut down container composition${NC}"
-docker compose down
 
 # Pull image update from docker hub
 echo -e "${PURPLE}Pull image update from docker hub${NC}"
