ecf8e417e94c971a1e4e0ac57c7c6968f4b9c37f
Stephan Becker <stephan.becker@becker-systemberatung.de>
2022-11-07 13:22:42 +0100

    dynamic master - Cluster and Gateway Master handling

--- nexus_wipe_messages.sh
+++ nexus_wipe_messages.sh
@@ -1,26 +1,12 @@
 #!/bin/bash
 #################################################
-# Clear Nexus message store
+# Clear reticulum storage
 #
 source colors_set.sh
 
 # If no template name to be used to create .env use 'default'
 TEMPLATE=${1-default}
 
-# Bounce docker (here down) if option is given
-if [ "$1" == "-d" ] ; then
-  # Shut down actual container composition
-  echo -e "${PURPLE}Shut down actual container composition${NC}"
-  docker compose down
-fi
-
 # Remove Nexus Message Store file
-echo -e "${PURPLE}Remove ./nexus_root/.nexus/storage/messages.umspack${NC}"
-sudo rm -f ./nexus_root/.nexus/storage/messages.umsgpack
-
-# Bounce docker (here up) if option is given
-if [ "$1" == "-d" ] ; then
-  # Launch container composition
-  echo -e "${PURPLE}Launch container composition${NC}"
-  docker compose up -d
-fi
+echo -e "${PURPLE}Remove ./nexus_root/.reticulum/$TEMPLATE/storage${NC}"
+sudo rm -f -r "./nexus_root/.reticulum/$TEMPLATE/storage"
